e7679b4772e56adbcc68acfc50becae0975bd1d1
Laureano <laureanojuarez69@gmail.com>
2024-12-20 12:11:55 -0300

    Mi commit

--- src/renderer/src/components/navbar/navbar.jsx
+++ src/renderer/src/components/navbar/navbar.jsx
@@ -17,9 +17,6 @@ export const Navbar = () => {
         <li>
           <a href="/services">Services</a>
         </li>
-        <li>
-          <a href="/contact">Contact</a>
-        </li>
       </ul>
     </NavbarStyled>
   )
